37643c71996818b041b93c7ad0852d185d1545d8
Tamilarasan143 <tamilarasan1432000@gmail.com>
2022-12-08 18:42:03 +0530

    Update

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className="container">
+                <p className="float-end"><a href="#">Back to top</a></p>
+                <p>&copy; 2021–2022 Company, Jsoft. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
+        </div>
+    )
+}
